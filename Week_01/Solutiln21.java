c27d543d5a7e21d0ac49352786ded1c0fba30ff5
张文彬 <zhangwbin@yongyou.com>
2020-05-20 21:31:24 +0800

    修改方法名

--- Week_01/Solutiln21.java
+++ Week_01/Solutiln21.java
@@ -2,17 +2,17 @@
 
 public class Solution21{
 
-  public ListNode mergeListNode(ListNode l1, ListNode l2){
+  public ListNode mergeTwoLists(ListNode l1, ListNode l2){
    /* // 递归法
     if (l1 == null) {
       return l2;
     } else if (l2 == null) {
       return l1;
     } else if (l1.val < l2.val) {
-      l1.next = mergeListNode(l1.next, l2);
+      l1.next = mergeTwoLists(l1.next, l2);
       return l1;
     } else {
-      l2.next = mergeListNode(l1, l2.next);
+      l2.next = mergeTwoLists(l1, l2.next);
       return l2;
     }*/
     // 迭代法
