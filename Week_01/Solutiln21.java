d8ae5a84de018c6c34da7b68384d4bbc5ec5fdc7
张文彬 <zhangwbin@yongyou.com>
2020-05-23 23:03:38 +0800

    添加注释

--- Week_01/Solutiln21.java
+++ Week_01/Solutiln21.java
@@ -2,16 +2,26 @@
 
 public class Solution21{
 
+  /**
+   * 合并两个链表
+   * @param l1
+   * @param l2
+   * @return
+   */
   public ListNode mergeTwoLists(ListNode l1, ListNode l2){
    /* // 递归法
     if (l1 == null) {
+    // 判断l1为空，返回l2
       return l2;
     } else if (l2 == null) {
+     // 如果l2为空，返回l1
       return l1;
     } else if (l1.val < l2.val) {
+    //如果l1的val小于l2的val，l1的next继续递归判断l1的next与l2谁小，最终返回l1
       l1.next = mergeTwoLists(l1.next, l2);
       return l1;
     } else {
+    // 如果l2.val小于等于l1.val，l2.next继续递归判断l2.next与l1谁小，最终返回l2
       l2.next = mergeTwoLists(l1, l2.next);
       return l2;
     }*/
--- Week_01/Solution26.java
+++ Week_01/Solution26.java
@@ -7,17 +7,20 @@ public class Solution26{
    * @return
    */
   public int removeDuplicates(int[] nums) {
-
+    // 如果nums是空，返回0
     if (nums == null) {
       return 0;
     }
+    // 定义数组当前非重复元素所在位置
     int i = 0;
     for (int j = 1; j < nums.length; j++) {
+      // 如果当前重复元素跟当前元素不相同，说明到了下一个重复元素，将下标i++，并且设置nums[i]的值等于当前元素的值
       if (nums[i] != nums[j]) {
         i++;
         nums[i] = nums[j];
       }
     }
+    // 返回当前元素的长度，需要+1
     return i + 1;
   }
 
